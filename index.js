698cf4ee38a78f8fab76b59685c03115e75fdea7
dmitrii92 <fogot92@gmail.com>
2020-10-12 13:44:25 +0300

    fix: error handling

--- index.js
+++ index.js
@@ -13,26 +13,30 @@ app.get("/plot", (req, res) => {
   waApi
     .getFull({ i: req.query.expression, plotwidth: 400, width: 400 })
     .then((wolframData) => {
-      const pods = wolframData.pods;
+      if (wolframData.success === true) {
+        const pods = wolframData.pods;
+        console.log(wolframData);
+        let img;
+        pods.map((pod) => {
+          if ("Plot" === pod.title) {
+            pod.subpods.map((subpod) => {
+              img = {
+                imgSrc: subpod.img.src,
+                imgWidth: subpod.img.width,
+                imgHeigth: subpod.img.heigth,
+              };
+              return;
+            });
+          }
+        });
 
-      let img;
-      pods.map((pod) => {
-        if ("Plot" === pod.title) {
-          pod.subpods.map((subpod) => {
-            img = {
-              imgSrc: subpod.img.src,
-              imgWidth: subpod.img.width,
-              imgHeigth: subpod.img.heigth,
-            };
-            return;
-          });
-        }
-      });
-
-      res.send(img);
+        res.send(img);
+      } else {
+        res.status(500).send("Wrong expression!");
+      }
     })
     .catch((reason) => {
-      res.send(reason);
+      res.status(500).send(reason);
     });
 });
 
